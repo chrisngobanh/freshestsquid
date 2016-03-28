d87e74277ac0b3917a41d0d17ed706ea012fb139
Christopher Banh <chrisngobanh@gmail.com>
2016-03-27 21:23:37 -0700

    Create new global component: Message
    Define global component: a component that can be reused by any other
    component, regardless of which menu/page it is on. e.g. Image, Logout
    Button
    
    Message is a component whose purpose is to relay information to the user
    e.g. "That username has already been taken!.", "Loadout deleted."

---
+++ shared/views/Message.jsx
@@ -0,0 +1,56 @@
+import React, { PropTypes } from 'react';
+
+import BaseComponent from './BaseComponent';
+import { Motion, spring } from 'react-motion';
+
+const propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+const store = { open: true };
+
+class Message extends BaseComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = store;
+
+    this.bind('goBack');
+  }
+
+  componentDidMount() {
+    // When the message is shown, give it 10 seconds before hiding it
+    setTimeout(() => this.goBack(), 10000);
+  }
+
+  goBack() {
+    store.open = false;
+    this.setState(store);
+  }
+
+  render() {
+    return (
+      <div>
+        <Motion style={{ x: spring(this.state.open ? -700 : 0) }}>
+          {({ x }) =>
+            <div
+              className="message"
+              onClick={this.goBack}
+              style={{
+                WebkitTransform: `translate3d(${x}px, 0, 0)`,
+                transform: `translate3d(${x}px, 0, 0)`,
+              }}
+            >
+              <span className="message-close">&times;</span>
+              <span className="message-txt">{ this.props.text }</span>
+            </div>
+          }
+        </Motion>
+      </div>
+    );
+  }
+}
+
+Message.propTypes = propTypes;
+
+export default Message;
